b0aa99a12a9151019a1a704541e604ae27dd02e0
Patrik Tomek <pataz23@seznam.cz>
2022-09-26 16:36:29 +0200

    API fetch, useState for ID and Quote

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [quote, setQuote] = useState([]);
+  const [id, setId] = useState([]);
+
+  const fetchAdvice = async () => {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+
+    setQuote(data.slip.advice);
+    setId(data.slip.id);
+  };
+
   return (
     <div className="container">
       <div className="app-container">
-        <h1 className="advice-id">* ADVICE ID *</h1>
-        <p className="quote"> "avdssd vasvv dsacd cdcsa ca caccdccd cdda saxccacscd"</p>
+        <h1 className="advice-id">ADVICE #{id}</h1>
+        <p className="quote">"{quote}"</p>
         <div className="pattern-divider"></div>
-        <button className="random-advice_btn"></button>
+        <button className="random-advice_btn" onClick={fetchAdvice}></button>
       </div>
     </div>
   );
